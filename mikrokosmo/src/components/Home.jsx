0fcdb12a673517ebc708e318746f21a77dcd70f1
Beca <234265987+becsnr@users.noreply.github.com>
2026-04-23 13:40:01 -0300

    mobile

--- mikrokosmo/src/components/Home.jsx
+++ mikrokosmo/src/components/Home.jsx
@@ -1,17 +1,21 @@
 import styles from './Home.module.css'
 
-import ice from '../assets/ice-cream-home.png'
+import ice from '../assets/ice-star.png'
 
 function Home() {
     return (
         <>
             <section id='home' className={styles.home}>
-                <div className={styles.container}>
-                    
-                </div>
-
                 <div className={styles.imagem}>
                     <img src={ice} alt="" />
+                </div>
+                
+                <div className={styles.slogan}>
+                    <h1>Doces que são de <span>outro mundo</span></h1>
+
+                    <p>Sorvetes artesanais, sobremesas irresistíveis e muito sabor em cada detalhe.</p>
+
+                    <button>Ver cardápio</button>
                 </div>
             </section>
         </>
