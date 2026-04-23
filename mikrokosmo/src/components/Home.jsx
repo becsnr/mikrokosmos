ef8858c169cc934e3ced0f42456edf021dbc8574
Beca <234265987+becsnr@users.noreply.github.com>
2026-04-23 09:12:07 -0300

    nav update

--- mikrokosmo/src/components/Home.jsx
+++ mikrokosmo/src/components/Home.jsx
@@ -1,6 +1,5 @@
 import styles from './Home.module.css'
 
-import planeta from '../assets/logo-planeta.png'
 import ice from '../assets/ice-cream-home.png'
 
 function Home() {
@@ -8,20 +7,7 @@ function Home() {
         <>
             <section id='home' className={styles.home}>
                 <div className={styles.container}>
-                    <h1 className={styles.titulo}>
-                        Mikrokosm
-                        <span className={styles.planeta}>
-                            <img src={planeta} alt="" />
-                            <span className={styles.star1}></span>
-                        </span>
-                        s
-                    </h1>
-                    <h2 className={styles.subtitulo}>
-                        <span></span>
-                        <p>sweets</p>
-                        <span></span>
-                    </h2>
-                    {/* <span className={styles.star2}></span> */}
+                    
                 </div>
 
                 <div className={styles.imagem}>
--- mikrokosmo/src/components/Navbar.jsx
+++ mikrokosmo/src/components/Navbar.jsx
@@ -1,12 +1,35 @@
 import styles from './Navbar.module.css'
 
+import planeta from '../assets/logo-planeta.png'
+
+import { FaBars } from 'react-icons/fa';
+
 function Navbar() {
     return (
         <nav className={styles.nav}>
-            <p>Home</p>
-            <p>Menu</p>
-            <p>Sobre</p>
-            <p>Contato</p>
+            <div className={styles.container}>
+                <h1 className={styles.titulo}>
+                    Mikrokosm
+                    <span className={styles.planeta}>
+                        <img src={planeta} alt="" />
+                    </span>
+                    s
+                </h1>
+                <h2 className={styles.subtitulo}>
+                    <span></span>
+                    <p>sweets</p>
+                    <span></span>
+                </h2>
+            </div>
+
+            <div className={styles.menuSVG}><FaBars /></div>
+            <div className={styles.menu}>
+                
+                <p>Home</p>
+                <p>Menu</p>
+                <p>Sobre</p>
+                <p>Contato</p>
+            </div>
         </nav>
     )
 }
