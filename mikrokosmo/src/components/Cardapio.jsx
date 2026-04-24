54f8eee7e9d87f4befe8c9827f341e50db81a5e6
Beca <234265987+becsnr@users.noreply.github.com>
2026-04-24 10:09:52 -0300

    botoes

--- mikrokosmo/src/components/Cardapio.jsx
+++ mikrokosmo/src/components/Cardapio.jsx
@@ -1,8 +1,16 @@
 import styles from './Cardapio.module.css'
 
+// import { IoCafe } from "react-icons/io5";
+// import { MdCandy } from "react-icons/md";
+// import { FaIceCream, FaBirthdayCake, FaGlassWhiskey } from "react-icons/fa";
+// import { GiMilkshake } from "react-icons/gi";
+
+
 import pink from '../assets/pink-planet.png'
 import purple from '../assets/purple-planet.png'
 import stars from '../assets/stars.png'
+
+import Card from './Card'
 
 function Cardapio() {
     return (
@@ -17,6 +25,29 @@ function Cardapio() {
                 </div>
 
                 <img src={purple} className={styles.purplePlanet} />
+            </div>
+
+            <div className={styles.btns}>
+                <button>
+                    Destaques
+                </button>
+                <button>
+                    {/* <FaIceCream /> */}
+                    Sorvetes
+                </button>
+                <button>
+                    {/* <FaBirthdayCake /> */}
+                    Bolos
+                </button>
+                <button>
+                    {/* <MdCandy /> */}
+                    Doces
+                </button>
+                <button>
+                    {/* <IoCafe /> */}
+                    {/* <FaGlassWhiskey /> */}
+                    Bebidas
+                </button>
             </div>
         </section>
     )
